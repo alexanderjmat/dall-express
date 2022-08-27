77d7c1f0f7409645004cf84cb27c3e5e96d3c4d5
alexanderjmat <alexanderjmatula@gmail.com>
2022-08-27 13:59:15 -0400

    draft of art gallery

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask, request, render_template,  redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from psycopg2 import connect
+from models_and_functions import models
+from werkzeug.utils import secure_filename
+import bcrypt
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///dallexpress'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'aj1234'
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
+
+models.connect_db(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
